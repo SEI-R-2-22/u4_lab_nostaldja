e111333054ccf743d75484a70beeaaf5e1190da0
Jenna Leopold <jleopold424@gmail.com>
2022-05-03 18:36:14 -0400

    lab/hw

---
+++ nostaldja_django/nostaldja/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Decade(models.Model):
+    start_year = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.start_year
+
+
+class Fad(models.Model):
+    name = models.CharField(max_length=500)
+    image_url = models.TextField()
+    description = models.CharField(max_length=500)
+    decade = models.ForeignKey(
+        Decade, on_delete=models.CASCADE, related_name='fads')
