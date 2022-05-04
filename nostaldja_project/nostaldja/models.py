20475c2c50065deb4c5782f74f5e222b394afce5
Job85 <julianjernigan@ymail.com>
2022-05-03 23:33:24 -0400

    ran migrations and created seed migration

--- nostaldja_project/nostaldja/models.py
+++ nostaldja_project/nostaldja/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Decade(models.Model):
+    start_year = models.IntegerField()
+
+
+class Fad(models.Model):
+    name = models.CharField(max_length=50)
+    image_url = models.TextField()
+    description = models.TextField()
+    decade = models.CharField(max_length=10)
