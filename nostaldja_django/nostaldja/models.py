8277704af310cd053c766f3785a42e06451f82ae
Nick Lopez <nicklopez5454@yahoo.com>
2022-05-03 19:41:05 -0500

    many updates etc

---
+++ nostaldja_django/nostaldja/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Decade(models.Model):
+    start_year = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.start_year
+
+
+class Fads(models.Model):
+    name = models.CharField(max_length=500)
+    image_url = models.TextField()
+    description = models.CharField(max_length=500)
+    decade = models.ForeignKey(
+        Decade, on_delete=models.CASCADE, related_name='fads'
+    )
