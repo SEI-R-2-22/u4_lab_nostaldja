4acd550d1719ee838ac92b6573f36e1370e3cb47
Dan Ventura <danice.ventura@gmail.com>
2022-05-03 20:52:11 -0600

    up to adding edits to views

--- nostaldja_django/nostaldja/urls.py
+++ nostaldja_django/nostaldja/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.decade_list, name='decade_list'),
-    path('fads/', views.fad_list, name='fad_list')
+    path('fads/', views.fad_list, name='fad_list'),
+    path('decades/<int:pk>', views.decade_detail, name='decade_detail'),
+    path('fads/<int:pk>', views.fad_detail, name='fad_detail'),
+    path('decades/new', views.decade_create, name='decade_create'),
+    path('fads/new', views.fad_create, name='fad_create'),
 ]
--- nostaldja_django/nostaldja/views.py
+++ nostaldja_django/nostaldja/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Decade, Fad
+from .forms import DecadeForm, FadForm
 
 # Create your views here.
 
@@ -12,3 +13,35 @@ def decade_list(request):
 def fad_list(request):
     fads = Fad.objects.all()
     return render(request, 'nostaldja/fad_list.html', {'fads': fads})
+
+
+def decade_detail(request, pk):
+    decade = Decade.objects.get(id=pk)
+    return render(request, 'nostaldja/decade_detail.html', {'decade': decade})
+
+
+def fad_detail(request, pk):
+    fad = Fad.objects.get(id=pk)
+    return render(request, 'nostaldja/fad_detail.html', {'fad': fad})
+
+
+def decade_create(request):
+    if request.method == 'POST':
+        form = DecadeForm(request.POST)
+        if form.is_valid():
+            decade = form.save()
+            return redirect('decade_detail', pk=decade.pk)
+    else:
+        form = DecadeForm()
+    return render(request, 'nostaldja/decade_form.html', {'form': form})
+
+
+def fad_create(request):
+    if request.method == 'POST':
+        form = FadForm(request.POST)
+        if form.is_valid():
+            fad = form.save()
+            return redirect('fad_detail', pk=fad.pk)
+    else:
+        form = FadForm()
+    return render(request, 'nostaldja/fad_form.html', {'form': form})
