91a70c87ae84efe5de4f2c412e7330242aa2d720
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-05-04 01:07:33 -0400

    finished

---
+++ settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE nostaldja;
+CREATE USER nostaluser1 WITH PASSWORD 'tunr';
+GRANT ALL PRIVILEGES ON DATABASE nostaldja TO nostaluser1;
---
+++ tunr/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from tunr.models import Fad
+from.models import Fad, Decade
+
+# Register your models here.
+
+
+admin.site.register(Fad)
+admin.site.register(Decade)
