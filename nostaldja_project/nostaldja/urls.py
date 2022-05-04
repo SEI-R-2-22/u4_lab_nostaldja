3915a6f2f0f3774741040837a6bbe2a853fe482a
Job85 <julianjernigan@ymail.com>
2022-05-04 01:28:08 -0400

    created url paths for fad_detail & decade_detail

--- nostaldja_project/nostaldja/urls.py
+++ nostaldja_project/nostaldja/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.decade_list, name='decade_list'),
     path('fads/', views.fad_list, name='fad_list'),
-
+    path('decades/<int:pk>', views.decade_detail, name='decade_detail'),
+    path('fads/<int:pk>', views.fad_detail, name='fad_detail')
 ]
