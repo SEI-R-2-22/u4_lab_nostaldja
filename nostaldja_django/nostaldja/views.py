e795759920bb243a331b06c7c2dfdf9cd6be3c12
Jenna Leopold <jleopold424@gmail.com>
2022-05-03 20:37:38 -0400

    more progress

--- nostaldja_django/nostaldja/views.py
+++ nostaldja_django/nostaldja/views.py
@@ -7,3 +7,18 @@ from .models import Decade, Fad
 def decade_list(request):
     decades = Decade.objects.all()
     return render(request, 'nostaldja/decade_list.html', {'decades': decades})
+
+
+def decade_detail(request, pk):
+    decade = Decade.objects.get(id=pk)
+    return render(request, 'nostaldja/decade_detail.html', {'decade': decade})
+
+
+def fad_list(request):
+    fads = Fad.objects.all()
+    return render(request, 'nostaldja/fad_list.html', {'fads': fads})
+
+
+def fad_detail(request, pk):
+    fad = Fad.objects.get(id=pk)
+    return render(request, 'nostaldja/fad_detail.html', {'fad': fad})
