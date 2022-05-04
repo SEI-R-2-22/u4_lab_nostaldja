d4d7d6fde7019d15abc5b7146557d357377b3ca7
canhvo16 <canhvo16@gmail.com>
2022-05-04 07:32:32 -0500

    show views

---
+++ nostaldja_project/nostaldja/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Decade, Fad
+
+admin.site.register(Decade)
+admin.site.register(Fad)
+
+# Register your models here.
---
+++ nostaldja_project/nostaldja/forms.py
@@ -0,0 +1,10 @@
+from socket import fromshare
+from django import forms
+from .models import Decade, Fad
+
+
+class DecadeForm(forms.ModelForm):
+
+    class Meta:
+        model = Decade
+        fields = ('start_year',)
