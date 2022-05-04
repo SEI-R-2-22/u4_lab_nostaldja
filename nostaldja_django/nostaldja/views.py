a3c06bffdc97ecfa26d65ad0292ea3a3262f1c34
Mark Ryan <markrryan99@gmail.com>
2022-05-03 18:16:47 -0700

    all crud added

--- nostaldja_django/nostaldja/views.py
+++ nostaldja_django/nostaldja/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Decade, Fad
 
+from .forms import DecadeForm, FadForm
+# from .forms import FadForm
 # Create your views here.
 
 def decade_list(request):
@@ -19,3 +21,53 @@ def decade_detail(request, pk):
 def fad_detail(request, pk):
     fad = Fad.objects.get(id=pk)
     return render(request, 'nostaldja/fad_details.html', {'fad': fad})
+
+def decade_create(request):
+    if request.method == 'POST':
+        form = DecadeForm(request.POST)
+        if form.is_valid():
+            decade = form.save()
+            return redirect('decade_details', pk=decade.pk)
+    else:
+        form = DecadeForm()
+    return render(request, 'nostaldja/decade_form.html', {'form': form})
+
+def fad_create(request):
+    if request.method == 'POST':
+        form = FadForm(request.POST)
+        if form.is_valid():
+            fad = form.save()
+            return redirect('fad_details', pk=fad.pk)
+    else:
+        form = FadForm()
+    return render(request, 'nostaldja/fad_form.html', {'form': form})
+
+def decade_edit(request, pk):
+    decade = Decade.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = DecadeForm(request.Post, instance=decade)
+        if form.is_valid():
+            decade = form.save()
+            return redirect('decade_detail', pk=decade.pk)
+    else:
+        form = DecadeForm(instance=decade)
+    return render(request, 'nostaldja/decade_form.html', {'form': form})
+
+def fad_edit(request, pk):
+    fad = Fad.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = FadForm(request.Post, instance=fad)
+        if form.is_valid():
+            fad = form.save()
+            return redirect('fad_detail', pk=fad.pk)
+    else:
+        form = FadForm(instance=fad)
+    return render(request, 'nostaldja/fad_form.html', {'form': form})
+
+def decade_delete(request, pk):
+    Decade.objects.get(id=pk).delete()
+    return redirect('decade_list')
+
+def fad_delete(request, pk):
+    Fad.objects.get(id=pk).delete()
+    return redirect("fad_list")
